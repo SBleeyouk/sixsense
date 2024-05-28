b4bbe7f9fddd2f496e596604e17cd360da9fef2f
nonce <jiminyoon66@gmail.com>
2024-05-28 22:18:36 +0900

    css정리

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './header.css';
-import logo from '../ui/logo.png'; // Ensure you have the logo image in your src directory
+import logo from '../ui/logo.svg'; // Ensure you have the logo image in your src directory
+import clientImg from '../ui/thumImg.svg';
 
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="Logo" className="logo" />
+        <div className="blank"></div>
+        <img src={logo} alt="Logo" className="logo" />
+        <img src={clientImg} alt="마이페이지" className="thumImg" />
     </header>
   );
 }
