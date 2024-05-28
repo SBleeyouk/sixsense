201ae8154e381eb508cf85343d563748dc2c3caf
nonce <jiminyoon66@gmail.com>
2024-05-28 22:18:36 +0900

    css정리

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { generateResponse } = require('./openAI');
 const { runReplicate } = require('./musicgen');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
