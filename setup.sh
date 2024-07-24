727544e339af942f323e90373a0ebff30935b046
SBleeyouk <sbleeyouk@gmail.com>
2024-07-24 15:06:43 +0900

    Update setup.sh to install Python dependencies

--- setup.sh
+++ setup.sh
@@ -2,12 +2,16 @@
 
 # python-backend 폴더로 이동하여 Flask 서버 시작
 cd python-backend
+pip install -r requirements.txt
 gunicorn app:app &
 
 # server 폴더로 이동하여 Node 서버 시작
 cd ../server
+npm install
 npm start &
 
 # client 폴더로 이동하여 빌드된 React 앱 서빙
 cd ../client
+npm install
+npm run build
 serve -s build
