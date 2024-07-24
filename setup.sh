3ab81bc0b255eacf6bfb9d3df5918e0e47d16540
SBleeyouk <sbleeyouk@gmail.com>
2024-07-24 15:35:03 +0900

    Fix requirements.txt path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# 루트 디렉토리에서 Python 패키지 설치
+pip3 install -r requirements.txt
+
 # python-backend 폴더로 이동하여 Flask 서버 시작
 cd python-backend
-pip3 install -r ../requirements.txt
 gunicorn app:app &
 
 # server 폴더로 이동하여 Node 서버 시작
