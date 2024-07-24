b748a07b0d39a1957911c6e1a4e04d4b7062637d
SBleeyouk <sbleeyouk@gmail.com>
2024-07-24 15:45:31 +0900

    Fix build error

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+# 루트 디렉토리에서 Python 패키지 설치
+pip3 install -r requirements.txt
+
+# client 폴더로 이동하여 React 앱 빌드
+cd client
+npm install
+npm run build
+
+# server 폴더로 이동하여 의존성 설치
+cd ../server
+npm install
+
 # python-backend 폴더로 이동하여 Flask 서버 시작
-cd python-backend
+cd ../python-backend
 gunicorn app:app &
 
 # server 폴더로 이동하여 Node 서버 시작
