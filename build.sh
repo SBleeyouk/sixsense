47f3e12743f794e77dabf8d5216cbe6e3b8665aa
SBleeyouk <sbleeyouk@gmail.com>
2024-07-24 15:41:16 +0900

    Update build and setup scripts for Heroku deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# 루트 디렉토리에서 Python 패키지 설치
+pip3 install -r requirements.txt
+
 # client 폴더로 이동하여 React 앱 빌드
 cd client
 npm install
@@ -11,6 +14,6 @@ npm install
 
 # python-backend 폴더로 이동하여 의존성 설치
 cd ../python-backend
-pip install -r requirements.txt
+pip3 install -r ../requirements.txt
 
 cd ..
--- setup.sh
+++ setup.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-# 루트 디렉토리에서 Python 패키지 설치
-pip3 install -r requirements.txt
-
 # python-backend 폴더로 이동하여 Flask 서버 시작
 cd python-backend
 gunicorn app:app &
 
 # server 폴더로 이동하여 Node 서버 시작
 cd ../server
-npm install
 npm start &
 
 # client 폴더로 이동하여 빌드된 React 앱 서빙
 cd ../client
-npm install
-npm run build
 serve -s build
