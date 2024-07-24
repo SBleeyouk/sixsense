73f8a7a4de29e0ba62cee0ba018955e27ad7ab34
SBleeyouk <sbleeyouk@gmail.com>
2024-07-24 12:50:27 +0900

    Add root package.json and update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ gunicorn app:app &
 
 # server 폴더로 이동하여 Node 서버 시작
 cd ../server
-npm start &  # This will run 'node server.js' as defined in the package.json
+npm start &
 
 # client 폴더로 이동하여 빌드된 React 앱 서빙
 cd ../client
